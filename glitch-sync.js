14a31416ccc9fc7ccc765851887ae03d32ca60bd
hat <jburns@chapman.edu>
2018-08-18 13:21:30 -0700

    Added config for sync

--- glitch-sync.js
+++ glitch-sync.js
@@ -1,3 +1,17 @@
-var nrc = require('node-run-cmd');
+debug = true
 
-nrc.run("GLITCH_PROJECT_ID='65cbe78c-c018-45cf-a47b-ef157907457c' GLITCH_TOKEN='***REMOVED***' GH_REPO='jhburns/val-bot' DEBUG=sync-glitch* ./node_modules/.bin/sync-glitch");
+var nrc = require('node-run-cmd');
+var config = require('./sync-config.json');
+
+var command = "";
+
+//Making the command, env vars need to be first to have sync work correctly
+command += "GLITCH_PROJECT_ID='" + config.projectID + "' ";
+command += "GLITCH_TOKEN='" + config.authorization + "' ";
+command += "GH_REPO='" + config.repo + "' ";
+if (debug) {
+    command += " DEBUG=sync-glitch*";
+}
+command += "./node_modules/.bin/sync-glitch";
+
+nrc.run(command);
