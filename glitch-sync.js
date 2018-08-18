39fd8c4e762287700965353a182566e41c2d282d
hat <jburns@chapman.edu>
2018-08-18 13:09:10 -0700

    fixing sync

--- glitch-sync.js
+++ glitch-sync.js
@@ -1,3 +1,3 @@
 var nrc = require('node-run-cmd');
 
-nrc.run("GLITCH_PROJECT_ID='65cbe78c-c018-45cf-a47b-ef157907457c' GLITCH_TOKEN='***REMOVED***' GH_REPO='jhburns/val-bot' DEBUG=sync-glitch* ./node_modules/.bin/sync-glitch./node_modules/.bin/sync-glitch");
+nrc.run("GLITCH_PROJECT_ID='65cbe78c-c018-45cf-a47b-ef157907457c' GLITCH_TOKEN='***REMOVED***' GH_REPO='jhburns/val-bot' DEBUG=sync-glitch* ./node_modules/.bin/sync-glitch");
