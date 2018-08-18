559fece234ea96238a6eab7a0ccf86d1a968112e
hat <jburns@chapman.edu>
2018-08-18 13:42:39 -0700

    Fixing sync

--- glitch-sync.js
+++ glitch-sync.js
@@ -3,6 +3,8 @@ debug = true
 var nrc = require('node-run-cmd');
 var config = require('./sync-config.json');
 
+const { exec } = require('child_process');
+
 var command = "";
 
 //Making the command, env vars need to be first to have sync work correctly
@@ -19,4 +21,19 @@ var dataCallback = function(data) {
 };
 
 console.log(command);
-nrc.run(command, { onData: dataCallback });
+
+exec(command, (err, stdout, stderr) => {
+    if (err) {
+        console.log("Error: could not run command, make sure that sync-config is correct");
+        return;
+    }
+
+    //if statements needed to prevent extra lines if no error or out
+    if (stdout !== "") {
+        console.log(stdout);
+    }
+
+    if (stderr !== "") {
+        console.log(stderr);
+    }
+});
