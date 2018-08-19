bb05637db06e510c9ccbfb9c3719574bd8a073e2
hat <jburns@chapman.edu>
2018-08-18 19:26:52 -0700

    Updated everything in project

--- bot.js
+++ bot.js
@@ -1,59 +1,117 @@
-var Discord = require('discord.io');
+/*
+ Logging block, using winston
+ Use logger.info("text"); for normal text and change .info to .error for error
+ */
 
-var logger = require('winston');
-var auth = require('./auth.json');
+const winston = require('winston');
 
-
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-
-
-// Initialize Discord Bot
-var bot = new Discord.Client({
-    token: auth.token,
-    autorun: true
+const logger = winston.createLogger({
+    format:  winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+    ),
+    transports: [
+        new winston.transports.Console()
+    ]
 });
 
-//Needed to get properly formatted run id
+/*
+ Non Discord Helpers,
+ They may be used by discord, but don't need it
+ */
+
 function randomID(low, high) {
     //seems dumb, but string zero is needed for padding output
     var zero = '0';
     var padding = zero.repeat(high.toString().length);
     var num = Math.floor(Math.random() * (high - low) + low);
-    
+
     return (padding+num).slice(-padding.length);
 }
 
-bot.on('ready', function (evt) {
+/*
+ Command class, to make adding new commands easier
+ */
+
+class Command {
+
+    //name should be string, oncall should be function
+    constructor(name, oncall) {
+        this.name = name;
+        this.oncall = oncall;
+        Command.all_commands.push(this);
+    }
+
+    static init() {
+        Command.all_commands = [];
+    }
+
+    static getCommands() {
+        return Command.all_commands;
+    }
+}
+
+//needs to be ran before any commands are made
+Command.init();
+
+/*
+ Main Discord BLock
+ */
+const Discord = require('discord.js');
+var auth = require('./auth.json');
+
+const bot = new Discord.Client({});
+
+bot.on('ready', () => {
     logger.info('Connected');
     logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-    logger.info('Run Instance ID: ' + randomID(0, 999999));
+    logger.info(bot.user.username + ' - userID (' + bot.user.id + ')');
+    logger.info('Run Instance ID: ' + '(' + randomID(0, 999999) + ')' + ' Up in: ' + process.uptime() + 'sec');
 });
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it needs to execute a command
-    // for this script it will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
+bot.on('error', (message) => {
+    logger.info(message);
+});
 
-        args = args.splice(1);
+//global so the on message function can use it
+var quotes_text = [];
 
-        switch(cmd) {
-            // !ping
-            case 'painting':
-                bot.sendMessage({ to: channelID, message: 'Busy Painting! ᶠʳᵉᵉ ᵐᵉᵉ' });
-                break;
-            case 'dink':
-                bot.sendMessage({ to: channelID, message: '*Get dinked on* https://i.imgur.com/977QXU5.gif' });
-                break;
-        }
-    } else if (message.indexOf("and") >= 4 && message.indexOf("and") <= message.length - 7 && message.length <= 30 && message.substring(0,1) != '`') {
-        bot.sendMessage({ to: channelID, message: '```' + message + ' and PAINTING!```\n'});
+// Second on ready is to get quotes text async
+bot.on('ready', () => {
+    const quotes_channel = bot.channels.find('name', "quotes");
+
+    if (quotes_channel != null) {
+        const quotes = quotes_channel.fetchMessages()
+            .then(function (messages) {
+                logger.info("Messages loaded");
+                messages.array().forEach( function(element) {
+                        quotes_text.push(element.content);
+                });
+            })
+            .catch(function (error) {
+                logger.error("Quote messages not loaded");
+                logger.error(error);
+            });
+    } else {
+        logger.info("Not quotes channel found");
     }
 });
+
+bot.on('message', async message => {
+    var text = message.content;
+
+    if (text.substring(0, 1) == '!') {
+        var args = text.substring(1).split(' ');
+        var cmd = args[0];
+        console.log(cmd);
+
+        console.log(args);
+    } else if (text.indexOf("and") >= 4 && text.indexOf("and") <= text.length - 7 && text.length <= 30 && text.substring(0, 1) != '`') {
+        message.channel.send('```' + text + ' and PAINTING!```');
+    }
+});
+
+
+
+// Needs to be last so other methods are pre-loaded
+bot.login(auth.token);
