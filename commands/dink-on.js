3a89ad3df755a6d377d4521ceed51e10e55efb57
jhburns <jburns@chapman.edu>
2019-04-05 19:16:31 -0700

    Testing production

--- commands/dink-on.js
+++ commands/dink-on.js
@@ -26,53 +26,60 @@ let dink_on = {
         }
         const broadcast = bot.createVoiceBroadcast();
 
-        voiceChannel.join().then(connection => {
-            let to_say = "get dinked on ";
-            to_say += message.content.substring(message.content.indexOf(' '), 150);
-
-            googleTTS(to_say, 'en', 1)
-                .then(function (url) {
-                    let options = {
-                        directory: "./sounds/",
-                        filename: "temptalk.mp3"
-                    };
-
-                    download(url, options, function (err) {
-                        if (err) {
-                            logger.error(err);
-                        }
-
-                        broadcast.playFile('sounds/diiiink.mp3');
-                        let dispatcher = connection.playBroadcast(broadcast, {volume: 0.4});
-
-                        dispatcher.on('start', function () {
-                            new Promise(function (resolve, reject) {
-
-                                setTimeout(() => {
-                                    broadcast.playFile('sounds/temptalk.mp3');
-                                    dispatcher = connection.playBroadcast(broadcast, {volume: 0.5});
-                                }, 3000);
-                            });
-
-                            new Promise(function (resolve, reject) {
-                                setTimeout(() => {
-                                    voiceChannel.leave();
-                                    on = false;
-                                }, 7000);
-                            });
-
-                        });
-                    });
-                })
-                .catch(function (err) {
-                    logger.error(err);
-                });
-
-        }).catch(function (err) {
-            logger.error(err)
-        });
+        voiceChannel.join().then( () => getSound(voiceChannel, broadcast) )
+            .catch(function (err) {
+                logger.error(err)
+            });
     }
 };
+
+function getSound(voiceChannel, broadcast) {
+    let to_say = "get dinked on ";
+    to_say += message.content.substring(message.content.indexOf(' '), 150);
+
+    googleTTS(to_say, 'en', 1)
+        .then( () => downloadFile(url, voiceChannel, broadcast) )
+        .catch(function (err) {
+            logger.error(err);
+        });
+}
+
+
+
+function downloadFile(url, voiceChannel, broadcast) {
+    let options = {
+        directory: "./sounds/",
+        filename: "temptalk.mp3"
+    };
+
+    download(url, options, function (err) {
+        if (err) {
+            logger.error(err);
+        }
+
+        broadcast.playFile('sounds/diiiink.mp3');
+        let dispatcher = connection.playBroadcast(broadcast, {volume: 0.4});
+
+        dispatcher.on('start', () => playFile(voiceChannel, broadcast));
+    });
+}
+
+function playFile(voiceChannel, broadcast) {
+    new Promise(function () {
+        setTimeout(() => {
+            broadcast.playFile('sounds/temptalk.mp3');
+            dispatcher = connection.playBroadcast(broadcast, {volume: 0.5});
+        }, 3000);
+        });
+
+    new Promise(function () {
+        setTimeout(() => {
+            voiceChannel.leave();
+            on = false;
+        }, 7000);
+    });
+
+}
 
 
 module.exports = dink_on;
