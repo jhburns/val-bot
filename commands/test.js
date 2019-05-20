556b06c4b24b19e3f26e57a0a34af6984eec638a
jhburns <jburns@chapman.edu>
2019-05-19 18:07:45 -0700

    Finished test command comments

--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,7 @@
 let test = {
     name: "test",
     desc: "Used only to test bot in draft mode, when not making a new command",
-    draft: true, // Should always be true, I
+    draft: true, // Should always be true, Is only so new draft commands don't have to be created to test
     callback: function (message) {
         message.channel.send("Test working");
     }
