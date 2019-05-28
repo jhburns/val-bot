e6f9ecdfffacf42d5d228d4ae43271b5887fd226
jhburns <jburns@chapman.edu>
2019-05-27 19:34:15 -0700

    Lollipop command created

---
+++ commands/lollipop.js
@@ -0,0 +1,9 @@
+let lollipop = {
+    name: "chomp-on-lollipop",
+    desc: "only exists so peeps will thank of something to replace it",
+    callback: function (message) {
+        message.channel.send("**crunch crunch CRUNCH** - mmmh tastes so good, especially the blood and bone fragments!");
+    }
+};
+
+module.exports = lollipop;
