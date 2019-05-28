fef959b462e197f102a91521f8abb0d9a2ebff2e
jhburns <jburns@chapman.edu>
2019-05-27 19:37:40 -0700

    Improved lollipop command with emojis

--- commands/lollipop.js
+++ commands/lollipop.js
@@ -2,7 +2,7 @@ let lollipop = {
     name: "chomp-on-lollipop",
     desc: "only exists so peeps will thank of something to replace it",
     callback: function (message) {
-        message.channel.send("**crunch crunch CRUNCH** - mmmh tastes so good, especially the blood and bone fragments!");
+        message.channel.send("👄 -> 🍭 **crunch crunch CRUNCH** - mmmh tastes so good, especially the blood and bone fragments!");
     }
 };
 
