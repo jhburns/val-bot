f896556aff4e8d74f1f41b49b8d6337b7cd64893
jhburns <jburns@chapman.edu>
2019-08-13 12:19:56 -0700

    Removing draft

--- commands/quote.js
+++ commands/quote.js
@@ -3,7 +3,6 @@ let random = require("../util/randoms");
 let quote = {
     name: "quote",
     desc: "says a random quote from the quotes channel",
-    draft: true,
     callback: function (message, bot, quotes_text) {
         message.channel.send(">>> " + quotes_text[random.intOfMax(quotes_text.length)]);
     }
