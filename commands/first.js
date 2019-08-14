0b37d5013331510ef82823c5ef4c510277c122cc
jhburns <jburns@chapman.edu>
2019-08-13 18:06:55 -0700

    Replacing quotes for !first too

--- commands/first.js
+++ commands/first.js
@@ -2,7 +2,7 @@ let first = {
     name: "first",
     desc: "says the most recently posted quote from the quotes channel",
     callback: function (message, bot, quotes_text) {
-        message.channel.send("```" + quotes_text[quotes_text.length - 1] + "```");
+        message.channel.send(">>> " + quotes_text[quotes_text.length - 1]);
     }
 };
 
