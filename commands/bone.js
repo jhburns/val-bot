2884ccefa33038a5fd4746faad386a0366cb8911
jhburns <jburns@chapman.edu>
2019-11-11 09:23:12 -0800

    Adding b99 image

--- commands/bone.js
+++ commands/bone.js
@@ -2,7 +2,9 @@ let bone = {
     name: "bone",
     alias: "b",
     desc: "Brooklyn 99 reference.",
+    draft: true,
     callback: function (message) {
+        message.channel.send({files: ["./img/b99.png"]});
         message.channel.send("BONE");
     }
 };
