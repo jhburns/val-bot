9a8f5746b582b2dabcb31e1315288ab35931122c
jhburns <jburns@chapman.edu>
2019-11-11 09:24:22 -0800

    Removing draft

--- commands/bone.js
+++ commands/bone.js
@@ -2,7 +2,6 @@ let bone = {
     name: "bone",
     alias: "b",
     desc: "Brooklyn 99 reference.",
-    draft: true,
     callback: function (message) {
         message.channel.send({files: ["./img/b99.png"]});
         message.channel.send("BONE");
