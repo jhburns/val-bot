8bf9bbfbee8efe605fe7e8e80aa7f7d700423e64
jhburns <jburns@chapman.edu>
2020-02-13 21:21:40 -0800

    Like more fixing

--- commands/banish.js
+++ commands/banish.js
@@ -29,7 +29,7 @@ let banish = {
             }
         } else {
             message.channel.send("You really thought you could do that?");
-            banishUser(message, message.author, removedCommandName);
+            banishUser(message, message.author, `<@${ message.author.id }>`);
         }
     }
 };
