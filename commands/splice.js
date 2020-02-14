7a218c72aa3f9253e597b5dfc4eed8e22293c521
jhburns <jburns@chapman.edu>
2020-02-13 20:23:02 -0800

    Splice command allows messages to be copies

--- commands/splice.js
+++ commands/splice.js
@@ -3,7 +3,7 @@ let random = require("../util/randoms");
 let splice = {
     name: "splice",
     alias: "s",
-    desc: "mashes two quotes together",
+    desc: "mashes two quotes together, star it to have val-bot ",
     callback: function (message, bot, { quotes_text }) {
         const first_quote = quotes_text[random.intOfMax(quotes_text.length)].split(" ");
         const second_quote = quotes_text[random.intOfMax(quotes_text.length)].split(" ");
@@ -11,7 +11,23 @@ let splice = {
         const first_half = first_quote.slice(0, first_quote.length / 2).join(" ");
         const second_half = second_quote.slice(second_quote.length / 2, second_quote.length).join(" ");
 
-        message.channel.send(">>> " + first_half + " " + second_half);
+        const body = `>>> ${ first_half } ${ second_half }`;
+
+        message.channel.send(body)
+            .then((new_message) => {
+               new_message
+                   .react('⭐')
+                   .then(() => {
+                       const filter = (reaction) => {
+                           return reaction.emoji.name === '⭐';
+                       };
+
+                       new_message.awaitReactions(filter, { max: 1, time: 15000 })
+                           .then(() => {
+                               bot.channels.get("677726820438769674").send(body);
+                           })
+                   })
+            });
     }
 };
 
