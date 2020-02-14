e4384a173eb04b391c7ba9a5c8d67af1d584ccda
jhburns <jburns@chapman.edu>
2020-02-13 23:36:12 -0800

    Improving more descriptions

--- commands/quote.js
+++ commands/quote.js
@@ -3,7 +3,7 @@ let random = require("../util/randoms");
 let quote = {
     name: "quote",
     alias: "qu",
-    desc: "says a random quote from the quotes channel",
+    desc: "says a random quote from <#465772508671508490>",
     callback: function (message, bot, { quotes_text }) {
         message.channel.send(">>> " + quotes_text[random.intOfMax(quotes_text.length)]);
     }
