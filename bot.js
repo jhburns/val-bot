ddb61ed99498fa407a0bc43522ac89bf8109aa6b
jhburns <jburns@chapman.edu>
2020-06-18 00:58:26 -0700

    .

--- bot.js
+++ bot.js
@@ -112,7 +112,6 @@ bot.on('message', async message => {
 
         if (current_cmd.draft ^ !(flags.draft !== "")) {
             current_cmd.oncall(message, { bot, quotes_text, fighting_words_text });
-            message.channel.send("LGBTQ Rights! 🏳️‍🌈");
         } else if (flags.draft === "") {
             logger.info('"!' + cmd_name + '"' + " command ignored due to running in production mode.");
         } else {
@@ -134,7 +133,6 @@ bot.on('message', async message => {
             return element.interpolated_value === "bone"
         });
 
-        message.channel.send("LGBTQ Rights! 🏳️‍🌈");
         current_cmd.oncall(message, bot, { quotes_text, fighting_words_text });
     }
 });
