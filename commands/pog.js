eaa7ff7e5c0b707567caf581ed26b9ff6b738e29
jhburns <jburns@chapman.edu>
2020-10-05 15:19:31 -0700

    Fixing pog

--- commands/pog.js
+++ commands/pog.js
@@ -3,7 +3,7 @@ let pog = {
     alias: "a",
     desc: "posts every pog emoji",
     callback: function(message) {
-        const ancient = Math.random() < 0.5 ? "<:ancientpog:725602510328561675>" ? "<:ancientpog2:725822760181563466>";
+        const ancient = Math.random() < 0.5 ? "<:ancientpog:725602510328561675>" : "<:ancientpog2:725822760181563466>";
 
         message.channel.send(`<:dinopog:698084158387519579> <:negapog:761287466119594034> ${ ancient } " +
             "<:jellopog:743528157612605482> <:meatpog:762165676093603860> <:usapog:758618815960449035>  " +
