d612b6f45b722a3310d0f0748e3bcb23c6feb434
jhburns <jburns@chapman.edu>
2020-10-05 14:05:02 -0700

    fixing halp

---
+++ commands/pog.js
@@ -0,0 +1,10 @@
+let pog = {
+    name: "allpog",
+    alias: "a",
+    desc: "posts every pog emoji",
+    callback: function(message) {
+        message.channel.send(":meatpog: :dinopog: :ancientpog: :ancientpog2: :grimpog: :jellopog: :negapog: :usapog: :willpog:");
+    }
+};
+
+module.exports = pog;
