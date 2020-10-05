6cf3e312f6bfe4877cfdbf71d459084c68608b8f
jhburns <jburns@chapman.edu>
2020-10-05 15:13:03 -0700

    Fixing pog

--- commands/pog.js
+++ commands/pog.js
@@ -5,7 +5,7 @@ let pog = {
     callback: function(message) {
         message.channel.send("<:dinopog:698084158387519579> <:negapog:761287466119594034> <:ancientpog:725602510328561675> " +
             "<:jellopog:743528157612605482> <:meatpog:762165676093603860> <:usapog:758618815960449035> <:ancientpog2:725822760181563466> " +
-            "<:grimpog:719427604884619365> <:willpog:720125268085178388>");
+            "<:grimpog:719427604884619365> <:willpog:720125268085178388> <:kog:750246977098481716>");
     }
 };
 
