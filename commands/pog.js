33e6fe864b51cdaa5c84cbe3af3c878ddb42f418
jhburns <jburns@chapman.edu>
2020-10-11 16:18:55 -0700

    Fixing pog

--- commands/pog.js
+++ commands/pog.js
@@ -6,7 +6,7 @@ let pog = {
         const ancient = Math.random() < 0.5 ? "<:ancientpog:725602510328561675>" : "<:ancientpog2:725822760181563466>";
 
         message.channel.send(`<:dinopog:698084158387519579> <:freesmileypog:764598680941756446> ${ ancient } ` +
-            "<:jellopog:743528157612605482> <:meatpog:762165676093603860> <:usapog:758618815960449035>  " +
+            "<:jellopog:743528157612605482> <:meatpog:762165676093603860> " +
             "<:grimpog:719427604884619365> <:willpog:764611841912995881> <:kog:762459118057160745>");
     }
 };
