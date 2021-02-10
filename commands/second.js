fd65b00da401e64a178d761e2a5032ca23fda5c6
jhburns <jburns@chapman.edu>
2021-02-09 22:47:38 -0800

    .

--- commands/second.js
+++ commands/second.js
@@ -2,7 +2,7 @@ let second = {
     name: "second",
     alias: "2",
     desc: "says the second most recently posted quote from <#465772508671508490>",
-    callback: function (message) {
+    callback: function (message, { quotes_text }) {
         message.channel.send(">>> " + quotes_text[quotes_text.length - 2]);
     }
 };
