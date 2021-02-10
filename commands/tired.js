ba55cc5e46c0f503bc45ba9abd567c6740b24bd1
jhburns <jburns@chapman.edu>
2021-02-09 22:43:14 -0800

    .

--- commands/tired.js
+++ commands/tired.js
@@ -2,7 +2,6 @@ let tired = {
     name: "tired",
     alias: "t",
     desc: "posts every tired emoji 😫",
-    draft: true,
     callback: function(message) {
         message.channel.send("<:tiredofthisbone:753902224756834305> <:tiredofthisshit:720125303359537224> " +
             "<:tiredofthisdean:794519413298298880> 😫");
