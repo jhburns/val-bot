f771d19ac7c38a886772c218446553835f12e186
jhburns <jburns@chapman.edu>
2021-02-15 00:12:39 -0800

    .

--- commands/emojify.js
+++ commands/emojify.js
@@ -4,13 +4,13 @@ const charMap = {
     'a': ':regional_indicator_a:', 'b': '🅱️', 'c': ':regional_indicator_c:', 'd': ':regional_indicator_d:', 'e': ':regional_indicator_e:',
     'f': ':regional_indicator_f:','g': ':regional_indicator_g:', 'h': ':regional_indicator_h:', 'i': ':regional_indicator_i:', 'j': ':regional_indicator_j:',
     'k': ':regional_indicator_k:', 'l': ':regional_indicator_l:', 'm': ':regional_indicator_m:', 'n': ':regional_indicator_n:', 'o': ':regional_indicator_o:',
-    'p': ':regional_indicator_p:', 'q': ':regional_indicator_q:', 'r': ':regional_indicator_r:', 's': ':regional_indicator_s:', 't': ':regional_indicator_t:',
+    'p': '🅿️', 'q': ':regional_indicator_q:', 'r': ':regional_indicator_r:', 's': ':regional_indicator_s:', 't': ':regional_indicator_t:',
     'u': ':regional_indicator_u:', 'v': ':regional_indicator_v:', 'w': ':regional_indicator_w:', 'x': ':regional_indicator_x:', 'y': ':regional_indicator_y:',
     'z': ':regional_indicator_z:',
 
     '0': '0️⃣', '1': '1️⃣', '2': '2️⃣', '3': '3️⃣', '4': '4️⃣', '5': '5️⃣', '6': '6️⃣', '7': '7️⃣', '8': '8️⃣', '9': '9️⃣',
 
-    '!': '‼️', '?': '⁉️',
+    '!': '‼️', '?': '⁉️', ' ': '⬜',
 };
 
 
