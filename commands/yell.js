6aad5d341a87cd0d8f812a204ce1b88fd5f954d0
Jonathan Burns <jburns@chapman.edu>
2021-03-17 16:40:01 -0700

    Update yell.js

--- commands/yell.js
+++ commands/yell.js
@@ -1,10 +1,9 @@
 const audio = require("../util/audio");
 
 let yell = {
-    name: "yell2",
+    name: "yell",
     alias: "y",
     desc: "`text to say (| voice name)` yell given text in your voice channel, voice name is random unless given",
-    draft: true,
     callback: function (message, {bot}) {
         let spaceDelimited = message.cleanContent.replace("@", "").split(" ");
         spaceDelimited.shift();
