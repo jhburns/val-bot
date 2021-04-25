a0ea70c4b4045c0f87ad83cfa2a8b3a74921bda6
jhburns <jburns@chapman.edu>
2021-04-24 21:59:55 -0700

    Adding cancel check

---
+++ commands/cancheck.js
@@ -0,0 +1,17 @@
+let banished = require("../util/banished_list");
+
+let cancheck = {
+    name: "cancheck",
+    alias: "cc",
+    desc: "Check who is currently cancelled.",
+    draft: true,
+    callback: function (message) {
+        let cancelled = banished.map((u) => `<@${u}>`).join(", ");
+        if (banished.length === 0) {
+            cancelled = "nobody";
+        }
+        message.channel.send(`Currently cancelled: ${cancelled}.`);
+    }
+};
+
+module.exports = cancheck;
